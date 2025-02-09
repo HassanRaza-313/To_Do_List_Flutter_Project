a07ceb8283983991b18c7579cbc8b051a8f075c4
HassanRaza-313 <163582928+HassanRaza-313@users.noreply.github.com>
2025-02-09 16:03:27 +0500

    Add button

--- lib/home.dart
+++ lib/home.dart
@@ -16,25 +16,74 @@ class Home extends StatelessWidget{
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 50,),
-                    child: Text('To Do List',style: TextStyle(fontSize: 30,fontWeight: FontWeight.w500),),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50,),
+                        child: Text('To Do List',style: TextStyle(fontSize: 30,fontWeight: FontWeight.w500),),
+                      ),
+                      for(ToDo todoo in todosList)
+                      ToDoItem(todo: todoo,),
+                    ],
                   ),
-                  for(ToDo todoo in todosList)
-                  ToDoItem(todo: todoo,),
-                ],
-              ),
-            )
-          ],
-        ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 20,right: 20,left: 20),
+                      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5,),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0,
+                        ),],
+                        borderRadius: BorderRadius.circular(10),
+
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Add a new item ',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom:15,right: 10),
+                  child: ElevatedButton(
+                    child: Icon(Icons.add,color: Colors.white,size: 35),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                      primary: tdBlue,
+                      minimumSize: Size(30,70),
+                      elevation: 20,
+                    ),
+
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
